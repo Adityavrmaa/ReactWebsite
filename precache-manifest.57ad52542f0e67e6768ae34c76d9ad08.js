f82446b087fa88220f684ae27c6640891b41dc4d
Aditya verma <60606311+Adityavrmaa@users.noreply.github.com>
2021-01-19 14:04:46 +0530

    Updates

---
+++ precache-manifest.57ad52542f0e67e6768ae34c76d9ad08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e16379217259e075ab3cf1f4bf59d3",
+    "url": "/ReactWebsite/index.html"
+  },
+  {
+    "revision": "89847b6fbd7bf94f5a1c",
+    "url": "/ReactWebsite/static/css/main.2f47c37b.chunk.css"
+  },
+  {
+    "revision": "31a4a64a5344fee0ae11",
+    "url": "/ReactWebsite/static/js/2.a4993a93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactWebsite/static/js/2.a4993a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89847b6fbd7bf94f5a1c",
+    "url": "/ReactWebsite/static/js/main.6ec1d167.chunk.js"
+  },
+  {
+    "revision": "2f51b18709131093c34a",
+    "url": "/ReactWebsite/static/js/runtime-main.8398e043.js"
+  },
+  {
+    "revision": "3b4f5dc8189fa13013681f5a18862bde",
+    "url": "/ReactWebsite/static/media/1.3b4f5dc8.png"
+  },
+  {
+    "revision": "61f59952a39ec758591da2221a2d2298",
+    "url": "/ReactWebsite/static/media/2.61f59952.png"
+  }
+]);
